313d809dcd1fae8ee95863127b0359a793e3e9cc
Tarun Chaudhry <tarunc92@gmail.com>
2013-10-02 12:39:16 -0700

    got rid of the notification model; made observable cleaner

--- app/models/notification.js
+++
@@ -1,5 +0,0 @@
-Balanced.Notification = Balanced.Model.extend({
-	uri: ENV.BALANCED.NOTIFICATIONS + '/notifications?email='
-});
-
-Balanced.TypeMappings.addTypeMapping('notification', 'Balanced.Notification');
