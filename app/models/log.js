ed0dded96ab2495db4284a7fb7689dfefa9bc4b9
Tarun Chaudhry <tarunc92@gmail.com>
2013-12-03 14:20:23 -0800

    links

--- app/models/log.js
+++ app/models/log.js
@@ -3,7 +3,11 @@ Balanced.Log = Balanced.Model.extend({
 
 	short_url: function() {
 		return Balanced.Utils.stripDomain(this.get('message.request.url'));
-	}.property('log.message.request.url'),
+	}.property('message.request.url'),
+
+	link_to_url: function() {
+		return this.get('message.request.url');
+	}.property('message.request.url'),
 
 	condensed_request_url: function() {
 		return Balanced.Utils.prettyLogUrl(this.get('short_url'));
@@ -20,6 +24,44 @@ Balanced.Log = Balanced.Model.extend({
 			});
 		}
 	}.property('message.request.headers.X-Real-Ip'),
+
+	request_payload: function() {
+		return this.linkify('message.request.payload');
+	}.property('message.request.payload'),
+
+	response_body: function() {
+		return this.linkify('message.response.body');
+	}.property('message.response.body'),
+
+	linkify: function(k) {
+		var c = this.get(k);
+		var hash = {
+			customer: function (customerId, key) {
+				return 'customers/' + customerId;
+			},
+			owner_customer: function (customerId, key) {
+				return 'customers/' + customerId;
+			},
+		};
+
+		function isObject(obj) {
+			var newObj = {};
+
+			_.each(obj, function(val, key) {
+				if (hash[key]) {
+					newObj[key] = '<a href="'+ hash[key](val, key) + '">' + val + '</a>';
+				} else if (_.isObject(val)) {
+					newObj[key] = isObject(val);
+				} else {
+					newObj[key] = val;
+				}
+			});
+
+			return newObj;
+		};
+
+		return isObject(c);
+	}
 });
 
 Balanced.TypeMappings.addTypeMapping('log', 'Balanced.Log');
